0ab17838b11a743ae8610cad953ff147436da811
Daniel Bonifacio <contato@danielbonifacio.com.br>
2020-07-21 19:44:01 -0300

    add lifecycles

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
 class ClassCompoenent extends React.Component<{ name: string }> {
+  constructor(props: any) {
+    super(props)
+    console.log('constructor reached')
+  }
+
   state = {
     name: 'Mundo!!!!!!'
   }
+
+  componentDidMount() {
+    console.log('did mount reached')
+  }
+
+  componentDidUpdate() {
+    console.log('did update reached')
+  }
   
   render() {
+    console.log('render reached')
     return <div>
       <p>name: { this.state.name }</p>
       <button onClick={() => {
