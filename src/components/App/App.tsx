aafbcda8f84cb674586de909bebd73251fae6c8b
Daniel Bonifacio <contato@danielbonifacio.com.br>
2020-07-22 21:40:11 -0300

    import header component into app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from '../Header';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Header title="AlgaStock" />
+      dsadsada
     </div>
   );
 }
