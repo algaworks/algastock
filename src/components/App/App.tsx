5399e0a423aaf5c57ae64a71bedbf83155a2cba0
Daniel Bonifacio <contato@danielbonifacio.com.br>
2020-07-27 08:41:39 -0300

    Revert "add input component"
    This reverts commit 8907371accbef9bd212dfcf2fce1978f521ed688.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import Header from '../Header';
 import Button from '../../shared/Button';
 import Container from '../../shared/Container';
-import Input from '../../shared/Input';
 
 function TestComponent () {
   return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
@@ -21,7 +20,6 @@ function App() {
         >
           Alert
         </Button>
-        <Input />
       </Container>
     </div>
   );
