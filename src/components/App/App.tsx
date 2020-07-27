1c7c186765c336a89488687cacac312bf927ed22
Daniel Bonifacio <contato@danielbonifacio.com.br>
2020-07-27 10:13:50 -0300

    list render

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -17,18 +17,15 @@ function App() {
       <Header title="AlgaStock" />
 
       <Container>
-        <Button
-          onClick={() => window.alert('UIIIU')}
-          appendIcon={<TestComponent />}
-        >
-          Alert
-        </Button>
-        <Input
-          label="Street"
-          placeholder="E.g.: 15h Avenue"
-          value={street}
-          onChange={e => setStreet(e.target.value)}
-        />
+        <ul>
+          {
+            ['Daniel', 'William', 'Thiago', 'Daniel'].map((name, index) => {
+              return <li key={index}>
+                { name }
+              </li>
+            })
+          }
+        </ul>
       </Container>
     </div>
   );
