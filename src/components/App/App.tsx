adf590b02c9c3ca79a863f1d662eeffb42722859
Daniel Bonifacio <contato@danielbonifacio.com.br>
2020-07-27 10:32:34 -0300

    add table html structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from '../Header';
 import Container from '../../shared/Container';
+import Table from '../../shared/Table';
 
 function App() {
   return (
     <div className="App">
       <Header title="AlgaStock" />
       <Container>
-    
+        <Table />
       </Container>
     </div>
   );
--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
-import './Table.scss'
+// import './Table.scss'
 
 const Table = () => {
   return <table>
-
+    <thead>
+      <tr>
+        <th>Product</th>
+        <th>Price</th>
+        <th>Stock</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Cookie</td>
+        <td>$1.25</td>
+        <td>23</td>
+      </tr>
+      <tr>
+        <td>Milk 1L</td>
+        <td>$0.99</td>
+        <td>10</td>
+      </tr>
+    </tbody>
   </table>
 }
 
