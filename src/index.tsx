fef8a5c9a63237fa98af1d697259caa3bda8f465
Daniel Bonifacio <contato@danielbonifacio.com.br>
2020-08-03 18:27:44 -0300

    add axios to the project

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import http from './utils/http';
+
+http.get('/posts')
+http.get('/posts/1')
+http.get('https://viacep.com.br/ws/29140160/json/')
 
 ReactDOM.render(
   <React.StrictMode>
