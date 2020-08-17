9483364e2e0235685692488457cb792fc237ed92
Daniel Bonifacio <contato@danielbonifacio.com.br>
2020-08-17 10:59:19 -0300

    fix http interceptor

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,10 +1,17 @@
 import axios from 'axios'
+import { store } from '../redux'
 
 const http = axios.create({
   baseURL: 'http://localhost:3024',
-  headers: {
-    authorization: 'Bearer 123'
-  }
+})
+
+http.interceptors.request.use((config) => {
+  const token = store.getState().authentication.profile?.token
+
+  if (token)
+    config.headers.Authorization = `Bearer ${token}`
+
+  return config
 })
 
 export default http
